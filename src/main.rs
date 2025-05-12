40d67dec14aa52c341483a10079acf9287cd65d6
year221 <dhuang0413@gmail.com>
2025-05-11 22:14:53 -0400

    restructure code

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 use std::env;
-use std::fs;
+use std::process;
+use minigrep::Config;
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     dbg!(&args);
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err|{
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    dbg!(&query);
-    dbg!(&file_path);
-
-    let contents = fs::read_to_string(file_path).expect("should have been able to read the file");
-    dbg!(&contents);
-    println!("With text:\n{contents}");
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 
 }
